2dec88ff8713ca2f7cb6db545c8481067fd47f48
DávalosLeonardo <davaloslm@hotmail.com.ar>
2021-09-29 01:01:10 -0300

    Elimina parseInt innecesarios

--- site/src/controllers/productController.js
+++ site/src/controllers/productController.js
@@ -3,7 +3,7 @@ const productos = require("../data/productos.json");
 const controller = {
     product: (req, res)=> {
         const {id} = req.params;
-        const producto = productos.find(producto=>producto.id === id)
+        const producto = productos.find(producto=>producto.id === parseInt(id) )
         res.render("products/product", {producto} )
     },
 }
