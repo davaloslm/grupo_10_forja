fecbed987adf6d5e3acb3afd2050b798298fdbd8
RodriGBorges <gabii.borges1998@gmail.com>
2021-10-11 20:23:54 -0300

    Bug de archivos borrados

--- sprint2/routes/cart.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-const router = express.Router();
-
-router.get("/cart", (req, res)=> res.sendFile(path.join(__dirname, "..", "views", "cart.html")));
-
-
-
-module.exports = router;
