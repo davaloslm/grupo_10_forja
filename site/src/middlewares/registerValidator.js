ff3150d5bc25f2d4e6a6ad2b356be2c720a95399
DávalosLeonardo <davaloslm@hotmail.com.ar>
2021-10-25 00:06:44 -0300

    Agrega validaciones a registro

--- site/src/middlewares/registerValidator.js
+++ site/src/middlewares/registerValidator.js
@@ -1,3 +1,5 @@
+const usuarios = require("../data/users.json");
+const path = require("path");
 const {check} = require('express-validator');
 
 const registerValidator = [
@@ -12,7 +14,12 @@ check('apellido')
 check('email')
     .notEmpty().withMessage('El campo E-mail es obligatorio').bail()
     .isEmail().withMessage('El formato de E-mail debe ser válido').bail()
-    .trim(),
+    .trim().bail()
+    .custom((value)=>{
+        if(usuarios.find(usuario=>usuario.email === value)){
+            throw new Error("Este e-mail ya está registrado en nuestra base de datos")
+        } return true
+    }),
 check('fechaDeNac')
     .notEmpty().withMessage('El campo fecha de nacimiento es obligatorio').bail()
     .isDate().withMessage('El formato fecha debe ser válido'),
@@ -21,9 +28,29 @@ check('contraseña')
     .isLength({min: 6, max: 12}).withMessage('Tu contraseña debe tener un mínimo de 6 caracteres y un máximo de 12'),
 check('contraseña2')
     .notEmpty().withMessage('El campo contraseña es obligatorio').bail()
-    .isLength({min: 6, max: 12}).withMessage('Tu contraseña debe tener un mínimo de 6 caracteres y un máximo de 12'),
-    /* .equals().withMessage('Las contraseñas deben conincidir'), */
+    .isLength({min: 6, max: 12}).withMessage('Tu contraseña debe tener un mínimo de 6 caracteres y un máximo de 12').bail()
+    .custom(( value, {req})=>{
+        if(req.body.contraseña !== value){
+            throw new Error("Las contraseñas ingresadas no coinciden")
+        } return true
+    }),
+check('imagen')
+    .custom((value, {req})=>{
+        if(value){
+            let extensionesValidas = [".jpg", ".jpeg", ".png", ".gif"];
+            let imagen = req.file;
+            let imagenExtension = path.extname(imagen.originalname);
+            if(extensionesValidas.includes(imagenExtension) === false ){
+                throw new Error("Las extensiones permitidas son " + extensionesValidas.join(", "))
+            } 
+
+           
+        } return true
+    }),
+    /* .equals() */
 check('terminos')
     .notEmpty().withMessage('Debes aceptar los términos y condiciones')
 
 ];
+
+module.exports = registerValidator;
--- site/src/routes/usersRouter.js
+++ site/src/routes/usersRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multerUser = require('../middlewares/multerUser');
 const { cart, vistaRegistro, vistaLogin, login, userProfile, registro, cerrarSesion } = require("../controllers/usersController");
 const invitado = require('../middlewares/invitado');
+const registerValidator = require('../middlewares/registerValidator');
 
 
 /* GET users listing. */
@@ -11,7 +12,7 @@ router.get('/login', invitado, vistaLogin);
 router.post('/login',  login);
 
 router.get('/register', invitado, vistaRegistro);
-router.post('/register', multerUser.single('imagen'), registro); //Registro de usuarios / foto de perfil
+router.post('/register',  multerUser.single('imagen'), registerValidator, registro); //Registro de usuarios / foto de perfil
 
 
 router.get('/userProfile/:id',  userProfile);
