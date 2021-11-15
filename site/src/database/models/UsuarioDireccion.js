ed0e140e0e8b3036851b105a71c6e9dfed2f8c93
DávalosLeonardo <davaloslm@hotmail.com.ar>
2021-11-15 00:05:51 -0300

    Crea modelo UsuarioDireccion

---
+++ site/src/database/models/UsuarioDireccion.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+    let alias = "UsuarioDireccion"
+    
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        usuario_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        direccion_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+    } 
+    let config = {
+        tableName: 'usuario_direccion',
+        timestamps: false
+    }
+
+    const UsuarioDireccion = sequelize.define(alias, cols, config);
+
+    UsuarioDireccion.associate = (models) => {
+        UsuarioDireccion.belongsTo(models.Usuario, {
+            as: 'usuarios',
+            foreignKey: 'usuario_id'
+        })
+
+        UsuarioDireccion.belongsTo(models.Direccion, {
+            as: 'direcciones',
+            foreignKey: 'direccion_id'
+        })
+     
+    }
+
+    return UsuarioDireccion
+}
