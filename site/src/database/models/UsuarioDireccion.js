55a50dff8c5246d2861f0a8801512bd87f19872e
RodriGBorges <gabii.borges1998@gmail.com>
2021-11-16 23:43:37 -0300

    Subo modelo UsuarioDireccion

--- site/src/database/models/UsuarioDireccion.js
+++ site/src/database/models/UsuarioDireccion.js
@@ -1,41 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    let alias = "UsuarioDireccion"
-    
-    let cols = {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-        },
-        usuario_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        direccion_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        }
-    } 
-    let config = {
-        tableName: 'usuario_direccion',
-        timestamps: false
+  class UsuarioDireccion extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    UsuarioDireccion.belongsTo(models.Usuario, {
+        as: 'usuarios',
+        foreignKey: 'usuario_id'
+    })
+
+    UsuarioDireccion.belongsTo(models.Direccion, {
+        as: 'direcciones',
+        foreignKey: 'direccion_id'
+    })
     }
-
-    const UsuarioDireccion = sequelize.define(alias, cols, config);
-
-    UsuarioDireccion.associate = (models) => {
-        UsuarioDireccion.belongsTo(models.Usuario, {
-            as: 'usuarios',
-            foreignKey: 'usuario_id'
-        })
-
-        UsuarioDireccion.belongsTo(models.Direccion, {
-            as: 'direcciones',
-            foreignKey: 'direccion_id'
-        })
-     
-    }
-
-    return UsuarioDireccion
-}
+  };
+  UsuarioDireccion.init({
+    usuarioId: DataTypes.INTEGER,
+    direccionId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'UsuarioDireccion',
+  });
+  return UsuarioDireccion;
+};
