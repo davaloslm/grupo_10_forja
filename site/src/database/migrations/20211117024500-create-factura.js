812ebf684c6df337dcade53e184df025c0a8a930
RodriGBorges <gabii.borges1998@gmail.com>
2021-11-16 23:45:18 -0300

    Subo modelo Factura

---
+++ site/src/database/migrations/20211117024500-create-factura.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Facturas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fecha: {
+        type: Sequelize.DATE
+      },
+      usuarioId: {
+        type: Sequelize.INTEGER
+      },
+      total: {
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Facturas');
+  }
+};
