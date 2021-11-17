624bea769ee0a897870d6da2455071f1a4d61edc
RodriGBorges <gabii.borges1998@gmail.com>
2021-11-16 23:40:19 -0300

    Subo modelo ProductoTalle

---
+++ site/src/database/models/ProductoTalle.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ProductoTalle extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    ProductoTalle.belongsTo(models.Producto, {
+        as: 'productos',
+        foreignKey: 'producto_id'
+    })
+
+    ProductoTalle.belongsTo(models.Talle, {
+        as: 'categorias',
+        foreignKey: 'talle_id'
+    })
+    }
+  };
+  ProductoTalle.init({
+    productoId: DataTypes.INTEGER,
+    talleId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ProductoTalle',
+  });
+  return ProductoTalle;
+};
