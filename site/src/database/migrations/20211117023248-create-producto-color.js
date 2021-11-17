075334c7e14334bf24e60ffc6634bc0d0d868f1b
RodriGBorges <gabii.borges1998@gmail.com>
2021-11-16 23:33:15 -0300

    Subo modelo ProductoColor

---
+++ site/src/database/migrations/20211117023248-create-producto-color.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ProductoColors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productoId: {
+        type: Sequelize.INTEGER
+      },
+      colorId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ProductoColors');
+  }
+};
--- site/src/database/models/ProductoColor.js
+++ site/src/database/models/ProductoColor.js
@@ -1,41 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    let alias = "ProductoColor"
-    
-    let cols = {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-        },
-        producto_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        color_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        }
-    } 
-    let config = {
-        tableName: 'producto_color',
-        timestamps: false
+  class ProductoColor extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    ProductoColor.belongsTo(models.Producto, {
+        as: 'productos',
+        foreignKey: 'producto_id'
+    })
+
+    ProductoColor.belongsTo(models.Color, {
+        as: 'colores',
+        foreignKey: 'color_id'
+    })
     }
-
-    const ProductoColor = sequelize.define(alias, cols, config);
-
-    ProductoColor.associate = (models) => {
-        ProductoColor.belongsTo(models.Producto, {
-            as: 'productos',
-            foreignKey: 'producto_id'
-        })
-
-        ProductoColor.belongsTo(models.Color, {
-            as: 'colores',
-            foreignKey: 'color_id'
-        })
-     
-    }
-
-    return ProductoColor
-}
+  };
+  ProductoColor.init({
+    productoId: DataTypes.INTEGER,
+    colorId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ProductoColor',
+  });
+  return ProductoColor;
+};
