beef038abf1686f253ff7966ed256a62f1fa4a20
RodriGBorges <gabii.borges1998@gmail.com>
2021-11-19 00:04:51 -0300

    Seed de imagenes y correccion de direcciones

--- site/src/database/seeders/20211119004340-Direccion.js
+++ site/src/database/seeders/20211119004340-Direccion.js
@@ -1,25 +1,45 @@
 'use strict';
 const faker = require('faker');
 
-let direcciones = {
-  calle: faker.address.streetName,
-  numero: faker.address.countryCode,
-  localidad: faker.address.state,
-  provincia: faker.address.cityName,
-  codigoPostal: faker.address.countryCode,
-  departamento: 'No tiene'
+const faker = require('faker');
+const usuarios = require('./src/data/users.json')
+
+let direcciones = []
+for (let i = 0; i < usuarios.length; i++) {
+    
+    let direccion = {
+        calle: faker.address.streetName(),
+        numero: faker.datatype.number({
+            'min': 1,
+            'max': 10000
+        }),
+        localidad: faker.address.cityName(),
+        provincia: faker.address.state(),
+        codigoPostal: faker.datatype.number({
+        'min': 10000,
+        'max': 20000
+    }),
+        departamento: faker.datatype.number({
+        'min': 10,
+        'max': 50
+    })
+    }
+
+direcciones.push(direccion)
+
 }
+
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     
-  await queryInterface.bulkInsert('Direccion', direcciones, {});
+  await queryInterface.bulkInsert('Direcciones', direcciones, {});
   
   },
 
   down: async (queryInterface, Sequelize) => {
 
-  await queryInterface.bulkDelete('Direccion', null, {});
+  await queryInterface.bulkDelete('Direcciones', null, {});
     
   }
 };
