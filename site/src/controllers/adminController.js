30a45d47f72026cd0c24168b85755a42008b194a
DávalosLeonardo <davaloslm@hotmail.com.ar>
2021-11-23 18:54:03 -0300

    Actualiza funcion editar y eliminar

--- site/src/controllers/adminController.js
+++ site/src/controllers/adminController.js
@@ -155,7 +155,7 @@ const controller = {
                                     colorId: e
                                 }
                             )
-                            .then( () => console.log('Colors guardados satisfactoriamente'))
+                            .then( () => console.log('Colores guardados satisfactoriamente'))
                             .catch(error=> console.log(error))
                         })
                     }
@@ -202,82 +202,165 @@ const controller = {
     /////// Editar producto - Guardar ////////
 
     editar: (req, res)=> {
-        //falta imagen
+        
         const editProductErrors = validationResult(req);
         
         let productoEditado = productos.find(producto => producto.id === parseInt(req.params.id));
 
         if(editProductErrors.isEmpty()){
 
-            let {nombre, descripcion, precio, descuento, talle, color, categoria, envioGratis} = req.body
+            const {imagenes, nombre, marca,  descripcion, precio, stock, descuento, talle, color, categoria, envioGratis} = req.body
+
+            db.Producto.update({
+
+                nombre: nombre,
+                descripcion: descripcion,
+                precio: parseFloat(precio),
+                descuento: parseInt(descuento),
+                envio: envioGratis === undefined ? 0 : 1,
+                marca: marca,
+                stock: parseInt(stock)
     
-                productoEditado.envioGratis = envioGratis === undefined ? false : true;
-                productoEditado.descuento = descuento;
-                productoEditado.talle = typeof(talle) === 'string' ? [talle] : talle;
-                productoEditado.nombre = nombre;
-                productoEditado.descripcion = descripcion;
-                productoEditado.precio = precio;
-                productoEditado.color = [color];
-                productoEditado.categoria = typeof(categoria) === 'string' ? [categoria] : categoria;
+            },{
+                where: {id: req.params.id}
+            })
+            .then(producto =>{
+                //Imágenes//
+                if(req.files.length != 0){
+                    let images = req.files.map(image => {
+                        let item = {
+                            nombre : image.filename,
+                            productoId : producto.id
+                        }
+                        return item
+                    })
+
+                    var promesaImagenes = db.Imagen.bulkCreate(images)
+                        .then( () => console.log('Imágenes guardadas satisfactoriamente'))
+                        .catch(error=> console.log(error))
+                    }
+                
+                //Categoría//
+                if(typeof(categoria) === 'string'){
+                    var promesaCategoria = db.ProductoCategoria.create(
+                    {
+                        productoId: producto.id,
+                        categoriaId: parseInt(req.body.categoria)
+                    })
+                    .then( () => console.log('Categoría guardada satisfactoriamente'))
+                    .catch(error=> console.log(error))
+                }else{
+                    categoria.forEach(e=>{
+                        var promesaCategoria = db.ProductoCategoria.create(
+                            {
+                                productoId: producto.id,
+                                categoriaId: e
+                            }
+                        )
+                        .then( () => console.log('Categorías guardadas satisfactoriamente'))
+                        .catch(error=> console.log(error))
+                    })
+                }
+
+                //Talle//
+                if(typeof(talle) === 'string'){
+                    var promesaTalle = db.ProductoTalle.create(
+                        {
+                            productoId: producto.id,
+                            talleId: parseInt(req.body.talle)
+                        })
+                        .then( () => console.log('Talle guardado satisfactoriamente'))
+                        .catch(error=> console.log(error))
+                    }else{
+                        talle.forEach(e=>{
+                            var promesaTalle = db.ProductoTalle.create(
+                                {
+                                    productoId: producto.id,
+                                    talleId: e
+                                }
+                            )
+                            .then( () => console.log('Talles guardados satisfactoriamente'))
+                            .catch(error=> console.log(error))
+                        })
+                    }
+                //Color//
+                if(typeof(color) === 'string'){
+                    var promesaColor = db.ProductoColor.create(
+                        {
+                            productoId: producto.id,
+                            colorId: parseInt(req.body.color)
+                        })
+                        .then( () => console.log('Color guardado satisfactoriamente'))
+                        .catch(error=> console.log(error))
+                }else{
+                    color.forEach(e=>{
+                        var promesaColor = db.ProductoColor.create(
+                            {
+                                productoId: producto.id,
+                                colorId: e
+                            }
+                        )
+                        .then( () => console.log('Colores guardados satisfactoriamente'))
+                        .catch(error=> console.log(error))
+                    })
+                }
+
+                Promise.all([promesaImagenes, promesaCategoria, promesaTalle, promesaColor])
+                    .then( () => res.redirect("/product/" + producto.id))
+            })
+            .catch(error =>{
+                res.send("No se pudo editar el producto")
+                console.log(error);
+            })          
+                    
+            /* .then( result=>{
+                if (result[0] === 1) {
     
-                fs.writeFileSync(productosRuta, JSON.stringify(productos, null ,2))
+                    res.redirect(`/product/${+req.params.id}`)
     
-                res.redirect(`/product/${+req.params.id}`)
+                } else {
+                    res.redirect(`/product/${+req.params.id}`)
+                    
+                }
+            })
+            .catch(error =>{
+                res.???
+            }) */
+    
+            
+               
         } else {
             res.render('admin/edit', { errors: editProductErrors.mapped(), producto: productoEditado })
         }
 
-		}
-        
-        /* db.Producto.update({
-
-            nombre: nombre,
-            descripcion: descripcion,
-            precio: parseFloat(precio),
-            descuento: parseInt(descuento),
-            envio: envioGratis === undefined ? 0 : 1,
-            marca: marca,
-            stock: parseInt(stock)
-
-        },{
-            where: {id: req.params.id}
-        })
-        .then( result=>{
-            if (result[0] === 1) {
-
-                res.redirect(`/product/${+req.params.id}`)
-
-            } else {
-                res.redirect(`/product/${+req.params.id}`)
-                
-            }
-        })
-        .catch(error =>{
-            res.???
-        }) */,
+	},
 
         /////// Borrar producto ////////
 
-        eliminar: (req, res)=>{
+    eliminar: (req, res)=>{
+        db.Producto.destroy({
+            where: {id : req.params.id}
+        })
+        .then( result =>{
+            res.redirect("/admin")
+        })
+        .catch(error=>{
+            res.send("No se pudo eliminar el producto");
+            console.log(error);
+        })
+
+            
         
-            productos = productos.filter(e=>e.id !== parseInt(req.params.id))
+            /* productos = productos.filter(e=>e.id !== parseInt(req.params.id))
 
 
-         fs.writeFileSync(productosRuta, JSON.stringify(productos, null ,2))
+         fs.writeFileSync(productosRuta, JSON.stringify(productos, null ,2)) */
 
 		 res.redirect("/admin")
 
         }
 
-        /* db.Producto.destroy({
-            where: {id : req.params.id}
-        })
-        .then( result =>{
-            res.redirect("/admin")
-        }))
-        .catch(error=>{
-            res.???
-        }) */,
+        ,
 
         /////// administración de usuarios ///////
 
