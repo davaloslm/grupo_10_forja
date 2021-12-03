34d410410a9d649c773d38b6b69a6438528ff7a6
RodriGBorges <gabii.borges1998@gmail.com>
2021-12-03 00:31:21 -0300

    Muestro productos del home con funcion db

--- site/src/controllers/categoriasController.js
+++ site/src/controllers/categoriasController.js
@@ -6,12 +6,11 @@ const controller = {
         const productosDeLaCategoria = productos.filter(producto=>producto.categoria.includes(categoria) === true);
         const categoriaCorrecta = categoria;
         res.render("products/category", {productosDeLaCategoria, categoriaCorrecta} );
-       
 
 
         
     }
-  
+
 }
 
 module.exports = controller;
--- site/src/controllers/indexController.js
+++ site/src/controllers/indexController.js
@@ -1,13 +1,30 @@
-const productos = require("../data/productos.json");
-
+const { Op } = require('sequelize');
+const db = require('../database/models');
 
 const controller = {
-    index: (req, res, next)=>{
-        productosDestacados = productos.filter(e=>e.descuento === 0);
-        productosEnOferta = productos.filter(e=>e.descuento !== 0);
+    index: (req, res) => {
 
-        res.render('index', {productosDestacados, productosEnOferta});
-      },
+      let productosEnOferta = db.Producto.findAll({
+        where: { descuento: { [Op.gt]: 0 } },
+        include: [
+          {association: "imagen"}]
+      });
+      let productosDestacados = db.Producto.findAll({
+        where: {envio: 1},
+        include: [
+          {association: "imagen"}]
+      });
+
+      Promise.all([productosEnOferta, productosDestacados])
+          .then(([productos, productos2]) => {
+            res.render('index', { productosDestacados: productos2, productosEnOferta: productos });
+          })
+          .catch( error => {
+            res.send("Error al requerir productos de la base de datos")
+            console.log(error);
+          })
+    
+    },
     acceso: (req, res)=> {
       res.render('acceso')
     },
