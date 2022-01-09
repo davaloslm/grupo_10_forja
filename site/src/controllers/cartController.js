fc7ab084f7440476c68228e0723e9db6b25dcb25
DávalosLeonardo <davaloslm@hotmail.com.ar>
2022-01-09 16:23:54 -0300

    Agrega función agregar al carrito

---
+++ site/src/controllers/cartController.js
@@ -0,0 +1,51 @@
+const db = require('../database/models');
+const { Op } = require('sequelize');
+
+const controller = {
+    agregarAlCarrito: (req, res)=>{
+        let producto = req.params.prod;
+        let cantidad = parseInt(req.params.cantidad);
+
+        db.Carrito.findOne({
+            where:{
+                usuario_id: req.session.usuarioLogueado.id,
+                producto_id: producto
+            }
+        })
+        .then(result=>{
+            console.log(result);
+            if (result === null) {
+                db.Carrito.create({
+                    usuarioId: req.session.usuarioLogueado.id,
+                    productoId: producto,
+                    cantidad: cantidad
+                })
+                .then(carrito=>{
+                    res.render("users/cart", {carrito})
+                })
+                .catch(error=>{
+                    console.log(error);
+                    res.send("No se pudo crear el carrito")
+                })
+            } else {
+                db.Carrito.update({
+                    cantidad: result.cantidad + cantidad
+                }, {
+                    where: {
+                        usuarioId: req.session.usuarioLogueado.id,
+                        productoId: producto,
+                    }
+                })
+                .then(carrito=>{
+                    res.render("users/cart", {carrito})
+                })
+                .catch(error=>{
+                    console.log(error);
+                    res.send("No se pudo actualizar el carrito existente")
+                })
+            }
+        })
+    }
+}
+
+module.exports = controller;
